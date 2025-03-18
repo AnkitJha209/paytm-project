a51743539f41f5af4878182c0a802e5c9834438a
Ankit Jha <professionalaj209@gmail.com>
2025-03-18 20:39:34 +0530

    frontend domain added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://paytm-project-ruby.vercel.app/signin"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
